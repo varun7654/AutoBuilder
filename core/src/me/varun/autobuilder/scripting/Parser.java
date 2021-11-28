af6754fc9bdbb1ea3fd37b512105e03a43a05a4b
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-11-28 15:34:55 -0800

    redesign parser to be easier to read

--- core/src/me/varun/autobuilder/scripting/Parser.java
+++ core/src/me/varun/autobuilder/scripting/Parser.java
@@ -4,44 +4,27 @@ import java.util.List;
 
 public class Parser {
     public static boolean execute(String string, List<String> methods){
-        String[] commands = string.split("\n");
+        String[] commands = string.split("\n"); //Start by splitting everything by lines (commands)
         for (String command : commands) {
-            StringBuilder methodName = null;
-            StringBuilder argument = null;
-            for (int i = 0; i < command.length(); i++) {
-                char character = command.charAt(i);
-                if(argument != null){
-                    argument.append(character);
-                } else if(methodName == null){
-                    if(!Character.isWhitespace(character)){
-                        methodName = new StringBuilder().append(character);
-                    }
-
-                } else if(!Character.isWhitespace(character)){
-                    methodName.append(character);
-
-                } else {
-                    argument = new StringBuilder();
+            if(command.startsWith("#")) continue; //Ignore comments
+            String[] parts = command.split(" "); //Split by spaces
+            if(parts.length == 0) continue; //Ignore empty lines
+            String method = parts[0]; //Get the method name
+            if(!methods.contains(method)) return false; //Exit early if the method doesn't exist
+            String[] args = new String[parts.length - 1]; //Initialize the argument array
+            System.arraycopy(parts, 1, args, 0, parts.length - 1);
+            try {
+                switch (method) {
+                    case "print":
+                        if(args.length == 0) return false;
+                        break;
+                    case "sleep":
+                        Long.parseLong(args[0]);
+                        break;
+                    //Add more cases for other robot functions
                 }
-            }
-
-            System.out.println("Command " + command + " method name: " + methodName + " argument: " + argument);
-            if(methodName == null) return false;
-            switch (methodName.toString()){
-                case "print":
-                    if(argument == null) return false;
-                    break;
-                case "shootBalls":
-                case "setShooterSpeed":
-                case "sleep":
-                    try{
-                        Float.parseFloat(argument.toString());
-                    } catch (NumberFormatException | NullPointerException e){
-                        return false;
-                    }
-                    break;
-                default:
-                    if(!methods.contains(methodName.toString())) return false;
+            } catch (Exception e) {
+                return false;
             }
         }
         return true;
