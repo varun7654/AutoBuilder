876d1eea6b0b416ddbab503c42b4ec0541d88357
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-12-02 09:47:52 -0800

    use reflection to get methods

--- core/src/me/varun/autobuilder/scripting/Parser.java
+++ core/src/me/varun/autobuilder/scripting/Parser.java
@@ -1,8 +1,20 @@
 package me.varun.autobuilder.scripting;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class Parser {
+    private static final ArrayList<Function<String, Object>> TRY_PARSE_LIST;
+    static {
+        TRY_PARSE_LIST = new ArrayList<>();
+        TRY_PARSE_LIST.add((Integer::parseInt));
+        TRY_PARSE_LIST.add(Double::parseDouble);
+        //TRY_PARSE_LIST.add(Boolean::parseBoolean);
+        TRY_PARSE_LIST.add(String::valueOf);
+    }
+
     public static boolean execute(String string, List<String> methods){
         String[] commands = string.split("\n"); //Start by splitting everything by lines (commands)
         for (String command : commands) {
@@ -10,7 +22,7 @@ public class Parser {
             String[] parts = command.split(" "); //Split by spaces
             if(parts.length == 0) continue; //Ignore empty lines
             String method = parts[0]; //Get the method name
-            if(!methods.contains(method)) return false; //Exit early if the method doesn't exist
+            //if(!methods.contains(method)) return false; //Exit early if the method doesn't exist
             String[] args = new String[parts.length - 1]; //Initialize the argument array
             System.arraycopy(parts, 1, args, 0, parts.length - 1);
             try {
@@ -21,12 +33,50 @@ public class Parser {
                     case "sleep":
                         Long.parseLong(args[0]);
                         break;
-                    //Add more cases for other robot functions
+                    default:
+                        //Try using reflection
+                        try {
+                            String[] splitMethod = method.split("\\.");
+                            Class<?> cls = Class.forName("me.varun.autobuilder.util." + splitMethod[0]);
+
+                            Object[] objArgs = new Object[args.length];
+                            for (int i = 0; i < args.length; i++) {
+                                for (Function<String, Object> parser : TRY_PARSE_LIST) {
+                                    try {
+                                        objArgs[i] = parser.apply(args[i]);
+                                        break;
+                                    } catch (IllegalArgumentException e) {
+                                        continue;
+                                    }
+                                }
+                            }
+
+                            Class<?>[] argClasses = new Class[objArgs.length];
+                            for (int i = 0; i < objArgs.length; i++) {
+                                argClasses[i] = getPrimitiveClass(objArgs[i].getClass());
+                            }
+
+                            Object instance = cls.getMethod("getInstance").invoke(null);
+                            Method javaMethod = cls.getMethod(splitMethod[1], argClasses);
+
+                            javaMethod.invoke(instance, objArgs);
+                            break;
+                        } catch(Exception e) {
+                            return false;
+                        }
                 }
             } catch (Exception e) {
+                e.printStackTrace();
                 return false;
             }
         }
         return true;
+    }
+
+    private static Class<?> getPrimitiveClass(Class<?> cls) {
+        if(cls.equals(Integer.class)) return int.class;
+        if(cls.equals(Double.class)) return double.class;
+        if(cls.equals(String.class)) return String.class;
+        return cls;
     }
 }
--- core/src/me/varun/autobuilder/util/MathUtil.java
+++ core/src/me/varun/autobuilder/util/MathUtil.java
@@ -1,8 +1,16 @@
 package me.varun.autobuilder.util;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import me.varun.autobuilder.AutoBuilder;
+import me.varun.autobuilder.wpi.math.geometry.Pose2d;
 
 public class MathUtil {
+    private static MathUtil instance = new MathUtil();
+    public static MathUtil getInstance() {
+        return instance;
+    }
+
     public static float clamp(float val, float min, float max) {
         return Math.max(min, Math.min(max, val));
     }
@@ -18,4 +26,33 @@ public class MathUtil {
     public static float len2(Vector2 vec, float x, float y) {
         return ((x - vec.x) * (x - vec.x)) + ((y - vec.y) * (y - vec.y));
     }
+
+
+    public static Vector2 toRenderVector2(Pose2d poseMeters) {
+        return new Vector2((float) poseMeters.getTranslation().getX() * AutoBuilder.getConfig().getPointScaleFactor(),
+                (float) poseMeters.getTranslation().getY() * AutoBuilder.getConfig().getPointScaleFactor());
+    }
+
+    public static Vector3 toRenderVector3(Pose2d poseMeters) {
+        return new Vector3((float) poseMeters.getTranslation().getX() * AutoBuilder.getConfig().getPointScaleFactor(),
+                (float) poseMeters.getTranslation().getY() * AutoBuilder.getConfig().getPointScaleFactor(),
+                0);
+    }
+
+    public void printThing(String str) {
+        System.out.println("String: " + str);
+    }
+
+    public void printThing(int i) {
+        System.out.println("Int: " + i);
+    }
+
+    public void printThing(double b, double str) {
+        System.out.println("Double: " + b + " String: " + str);
+    }
+
+    public void addThing(double a, double b) {
+        System.out.println(a+b);
+    }
+
 }
