922cf6b43c13dd7042fe348efebf5a3b4c4519cf
Varun <varun7654@outlook.com>
2022-01-09 16:09:51 -0800

    Give the hover box a bit of space between it & the mouse

--- core/src/me/varun/autobuilder/gui/hover/HoverManager.java
+++ core/src/me/varun/autobuilder/gui/hover/HoverManager.java
@@ -48,7 +48,7 @@ public class HoverManager {
 
     public static void render(Batch batch, ShapeDrawer shapeDrawer) {
         if (hoverShown) {
-            if (hoverOnMouse) hoverPosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+            if (hoverOnMouse) hoverPosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY() + 4);
             float width = hoverText.getWidth();
             float height = hoverText.getHeight();
 
@@ -58,7 +58,7 @@ public class HoverManager {
                 hoverPosition.x = (width + 10) / 2;
             }
             if (hoverPosition.y + height + 5 > (flippedLastTime ? Gdx.graphics.getHeight() - 20 : Gdx.graphics.getHeight())) {
-                hoverPosition.y = hoverPosition.y - height - 5;
+                hoverPosition.y = hoverPosition.y - height - (hoverOnMouse ? 30 : 5);
                 flippedLastTime = true;
             } else {
                 flippedLastTime = false;
@@ -68,8 +68,7 @@ public class HoverManager {
                     width + 6, height + 6, Color.BLACK);
 
             shapeDrawer.filledRectangle(hoverPosition.x - ((width + 4) / 2),
-                    hoverPosition.y,
-                    width + 4, height + 4, Color.WHITE);
+                    hoverPosition.y, width + 4, height + 4, Color.WHITE);
 
             FontRenderer.renderText(batch, shapeDrawer,
                     hoverPosition.x - (width / 2),
