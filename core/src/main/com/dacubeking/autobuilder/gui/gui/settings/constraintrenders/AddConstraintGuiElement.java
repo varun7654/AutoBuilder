d1889d8e469411383784273cfa8f8c39c090f7da
varun7654 <varun7654@outlook.com>
2022-10-30 00:04:47 -0700

    adjust order of the add constraint gui element and render the plus icon to a bit more to the right

--- core/src/main/com/dacubeking/autobuilder/gui/gui/settings/constraintrenders/AddConstraintGuiElement.java
+++ core/src/main/com/dacubeking/autobuilder/gui/gui/settings/constraintrenders/AddConstraintGuiElement.java
@@ -7,7 +7,6 @@ import com.dacubeking.autobuilder.gui.AutoBuilder;
 import com.dacubeking.autobuilder.gui.gui.elements.scrollablegui.GuiElement;
 import com.dacubeking.autobuilder.gui.gui.elements.scrollablegui.TextGuiElement;
 import com.dacubeking.autobuilder.gui.gui.textrendering.TextComponent;
-import com.dacubeking.autobuilder.gui.undo.UndoHandler;
 import com.dacubeking.autobuilder.gui.util.Colors;
 import com.dacubeking.autobuilder.gui.util.MiscShapeRenderer;
 import com.dacubeking.autobuilder.gui.util.MouseUtil;
@@ -43,22 +42,25 @@ public class AddConstraintGuiElement implements GuiElement {
     }
 
     static {
+        constraints.add(new ConstraintType("Elliptical Region",
+                () -> new EllipticalRegionConstraint(new Translation2d(), 1, 1, new Rotation2d(), null)));
+        constraints.add(new ConstraintType("Rectangular Region",
+                () -> new RectangularRegionConstraint(new Translation2d(), new Translation2d(), null)));
+
         constraints.add(new ConstraintType("Centripetal Acceleration",
                 () -> new CentripetalAccelerationConstraint(1.0)));
+        constraints.add(new ConstraintType("Max Velocity",
+                () -> new MaxVelocityConstraint(1.0)));
+
         constraints.add(new ConstraintType("Differential Drive Kinematics",
                 () -> new DifferentialDriveKinematicsConstraint(new DifferentialDriveKinematics(10), 1.0)));
         constraints.add(new ConstraintType("Differential Drive Voltage",
                 () -> new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(1, 1, 1),
                         new DifferentialDriveKinematics(10), 10.0)));
-        constraints.add(new ConstraintType("Elliptical Region",
-                () -> new EllipticalRegionConstraint(new Translation2d(), 1, 1, new Rotation2d(), new MaxVelocityConstraint(1))));
-        constraints.add(new ConstraintType("Max Velocity",
-                () -> new MaxVelocityConstraint(1.0)));
+
         constraints.add(new ConstraintType("Mecanum Drive Kinematics",
                 () -> new MecanumDriveKinematicsConstraint(new MecanumDriveKinematics(
                         new Translation2d(), new Translation2d(), new Translation2d(), new Translation2d()), 1.0)));
-        constraints.add(new ConstraintType("Rectangular Drive Kinematics",
-                () -> new RectangularRegionConstraint(new Translation2d(), new Translation2d(), new MaxVelocityConstraint(1))));
         constraints.add(new ConstraintType("Swerve Drive Kinematics",
                 () -> new SwerveDriveKinematicsConstraint(new SwerveDriveKinematics(
                         new Translation2d(), new Translation2d(), new Translation2d(), new Translation2d()), 1)));
@@ -93,23 +95,22 @@ public class AddConstraintGuiElement implements GuiElement {
         public float render(@NotNull ShapeDrawer shapeRenderer, @NotNull PolygonSpriteBatch spriteBatch, float drawStartX,
                             float drawStartY, float drawWidth, Camera camera, boolean isLeftMouseJustUnpressed,
                             Consumer<TrajectoryConstraint> onAddConstraint) {
-            float textHeight = textGuiElement.getHeight(drawStartX + 20, drawStartY, drawWidth - 20, camera,
+            float textHeight = textGuiElement.getHeight(drawStartX + 25, drawStartY, drawWidth - 20, camera,
                     isLeftMouseJustUnpressed);
-            if (isMouseOver(drawStartX, drawStartY - 4 - textHeight, drawWidth, textHeight + 8)) {
+            if (isMouseOver(drawStartX, drawStartY - 9 - textHeight, drawWidth, textHeight + 8)) {
                 RoundedShapeRenderer.roundedRectTopLeft(shapeRenderer, drawStartX,
-                        drawStartY + 5, drawWidth, textHeight + 10, 5, Colors.LIGHT_GREY);
+                        drawStartY, drawWidth, textHeight + 10, 5, Colors.LIGHT_GREY);
 
                 if (MouseUtil.isIsLeftMouseJustUnpressed()) {
                     onAddConstraint.accept(trajectoryConstraintSupplier.get());
-                    UndoHandler.getInstance().reloadState();
                 }
             }
 
-            MiscShapeRenderer.plusIconCentered(shapeRenderer, drawStartX + 15, drawStartY - textHeight / 2, 16, 15,
+            MiscShapeRenderer.plusIconCentered(shapeRenderer, drawStartX + 20, drawStartY - (textHeight / 2) - 5, 16, 15,
                     Color.BLACK);
 
 
-            textGuiElement.render(shapeRenderer, spriteBatch, drawStartX + 20, drawStartY, drawWidth - 20, camera,
+            textGuiElement.render(shapeRenderer, spriteBatch, drawStartX + 20, drawStartY - 5, drawWidth - 20, camera,
                     isLeftMouseJustUnpressed);
 
             return textHeight + 10;
